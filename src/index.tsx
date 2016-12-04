6ef916e63ff11282f9dde990b7b5fe9fc0006a23
Ryan Clark <ryan@ryanclark.me>
2016-12-04 02:01:17 +0000

    chore: change from rynclark over to hayjs as it's now under an github org

--- src/index.tsx
+++ src/index.tsx
@@ -9,7 +9,7 @@ import Console from './_includes/console';
 import Footer from './_partials/footer';
 import Code from './_includes/code';
 
-export default class HomePage extends React.Component<{}, {}> {
+export default class Home extends React.Component<{}, {}> {
   render() {
     return (
       <div>

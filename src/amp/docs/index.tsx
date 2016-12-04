4b382c16f736e48f2370271373c95ee80c483fba
Ryan Clark <ryan@ryanclark.me>
2016-12-04 02:02:49 +0000

    feat(amp): add base for amp pages

--- src/amp/docs/index.tsx
+++ src/amp/docs/index.tsx
@@ -1,15 +1,16 @@
 /*---
 layout: amp
 canonical: https://hay.js.org/
-permalink: amp
+permalink: amp/docs
   ---*/
+
 import * as React from 'react';
 
-export default class HomeAMP extends React.Component<{}, {}> {
+export default class DocsAMP extends React.Component<{}, {}> {
   render() {
     return (
       <div>
-        test
+        test amp docs
       </div>
     );
   }
